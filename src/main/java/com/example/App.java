d97aed88251c5e88bce8d721ba4508bded63919a
lai2-24 <lai2-24@itismeucci.com>
2023-11-13 11:49:04 +0100

    second commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,30 @@
 package com.example;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            System.out.println("> Avvio del server....");
+            ServerSocket connectSocket = new ServerSocket(4500);
+            ArrayList <ServerThread> partecipanti = new ArrayList <ServerThread> ();
+
+            while(true){
+                Socket dataSocket = connectSocket.accept();
+                //System.out.println("> Si e' connesso un nuovo partecipante con Socket " + dataSocket.getRemoteSocketAddress());
+
+                ServerThread serverThread = new ServerThread(dataSocket, partecipanti);
+                partecipanti.add(serverThread);
+                serverThread.start();
+            }
+        } catch (Exception e) {
+            e.getMessage();
+            System.out.println("> Errore durante l'istanza del server!");
+        }
+
     }
 }
--- src/main/java/com/example/ServerThread.java
+++ src/main/java/com/example/ServerThread.java
@@ -1,5 +1,46 @@
 package com.example;
 
-public class ServerThread {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerThread extends Thread{
     
+    Socket clientSocket;
+    ArrayList <ServerThread> partecipanti;
+
+    public ServerThread(Socket s, ArrayList <ServerThread> array){
+        this.clientSocket = s;
+        this.partecipanti = array;
+    }
+
+    @Override
+    public void run(){
+        try {
+            BufferedReader inDalClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            DataOutputStream outVersoIlClient = new DataOutputStream(clientSocket.getOutputStream());
+            boolean esci = false;
+            String stringaInviata = "";
+            String stringaRicevuta = "";
+
+            stringaRicevuta = inDalClient.readLine();
+            System.out.println("Si e' connesso " + stringaRicevuta +  " !");
+
+
+            while(!esci){
+                stringaRicevuta = inDalClient.readLine();
+                if(stringaRicevuta.equals("-1")){
+                    esci = true;
+                }
+            }
+            clientSocket.close();
+
+        } catch (IOException e) {
+            e.getMessage();
+            System.out.println("Attenzione, errore nella creazione della comunicazione");
+        }
+    }
 }
